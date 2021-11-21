4ddcd68a8ea9f775985343ceb8e06b27e13392b0
carol santos <contato.caroline1999@gmail.com>
2021-11-20 22:31:13 -0300

    feat: created routes to endpóint courses

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { response } from 'express';
+
+@Controller('courses')
+export class CoursesController {
+  @Get()
+  findAll(@Res() response): string {
+    return response.status(200).send('Listagem de Curso');
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Curso # ${id} `;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body('name') body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `Atualização do Curso #${id} `;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Exclusão do Curso #${id} `;
+  }
+}
